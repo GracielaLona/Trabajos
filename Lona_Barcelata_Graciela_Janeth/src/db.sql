8f7a61d4fabfec16a8c72aa2864dd1536b288aef
GracielaLona <113053283+GracielaLona@users.noreply.github.com>
2023-03-06 18:23:03 -0600

    Add files via upload

---
+++ Lona_Barcelata_Graciela_Janeth/src/db.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3307
+-- Tiempo de generación: 01-03-2023 a las 23:57:46
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sistema`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `Email` varchar(50) NOT NULL,
+  `Nombre` varchar(100) NOT NULL,
+  `Apellido_materno` varchar(100) NOT NULL,
+  `Apellido_paterno` varchar(100) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Fecha_de_nacimiento` date NOT NULL,
+  `Teléfono` varchar(10) NOT NULL,
+  `Rol` varchar(1) NOT NULL,
+  `Status` int(11) NOT NULL,
+  `Activo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
